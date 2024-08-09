a465f06417317d39f29278b72bed0c9afdb0103b
Harsh-winuall <harsh@winuall.com>
2024-08-09 09:54:18 +0530

    Initialized the Git Repo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>This is Harsh Here!</h1>
+      <p>This is the Initial Version! 😃</p>
+    </div>
+  )
+}
+
+export default App
