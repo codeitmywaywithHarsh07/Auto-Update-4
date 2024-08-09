314019280b16203e90fa6c96ebf39f7628b403fe
Harsh-winuall <harsh@winuall.com>
2024-08-09 13:25:26 +0530

    Updated to  version v1.0.2

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>This is Harsh Here!</h1>
+      <h1>Heyya👋🏻 Harsh Here!</h1>
       <p>This is the Updated Version! 😃</p>
-      <h3>v1.0.1</h3>
+      <hr />
+      <h3>v1.0.2</h3>
     </div>
   )
 }
